8f441cdde1c9bb25ef1a0f80ae7a2af331cc839c
pletessier <pierre.letessier@gmail.com>
2020-02-02 18:05:24 +0100

    change version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='python-configuration-wrapper',
-    version='1.0.2',
+    version='1.0.3',
     description='A wrapper of python-configuration',
     url="https://github.com/pletessier/python-configuration-wrapper",
     long_description=long_description,
