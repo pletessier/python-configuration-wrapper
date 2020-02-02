3770cf8a3730bab851bd266c7c05aac4f461db99
pletessier <pierre.letessier@gmail.com>
2020-02-02 18:04:15 +0100

    add documentation to pypi.org

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='python-configuration-wrapper',
     version='1.0.2',
     description='A wrapper of python-configuration',
     url="https://github.com/pletessier/python-configuration-wrapper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Pierre Letessier',
     keywords=["configuration"],
     packages=['python_configuration_wrapper'],
