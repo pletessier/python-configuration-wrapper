e1329f63380bd10588ad76562ef4a316f222f550
pletessier <pierre.letessier@gmail.com>
2021-09-13 16:55:31 +0200

    Allow to add single quotes in keys

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='python-configuration-wrapper',
-    version='1.1.0',
+    version='1.2.0',
     description='A wrapper of python-configuration',
     url="https://github.com/pletessier/python-configuration-wrapper",
     long_description=long_description,
